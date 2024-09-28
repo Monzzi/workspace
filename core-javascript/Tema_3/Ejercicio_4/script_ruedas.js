c1aa91d180f4962227e31db50e0093a871984238
Monzzi <montsepilo@gmail.com>
2024-09-28 17:52:54 +0200

    Commit Ejericios 5 (los doce) del T.3

--- core-javascript/Tema_3/Ejercicio_4/script_ruedas.js
+++ core-javascript/Tema_3/Ejercicio_4/script_ruedas.js
@@ -9,9 +9,7 @@ if (diametro > 1.4) {
     console.log("La rueda es para un vehículo pequeño.");
 }
 
-if (diametro > 1.4 && grosor < 0.4) {       
-    console.log("El grosor para esta rueda es inferior al recomendado.");
-} else if (diametro <= 1.4 && diametro > 0.8 && grosor < 0.25) {
+if (diametro > 1.4 && grosor < 0.4) || ((diametro <= 1.4 && diametro > 0.8) && grosor < 0.25) {       
     console.log("El grosor para esta rueda es inferior al recomendado.");
 } else {
     console.log("Esta rueda está dentro de los parametros de seguridad.");
